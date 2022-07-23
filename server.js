80d5cff5666faf73612c70eccc5feb255f973c5c
teamwaahid <teamwaahid@gmail.com>
2022-07-23 23:32:14 +0600

    db bug fixed

--- server.js
+++ server.js
@@ -4,38 +4,35 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const {
-    notFoundHandler,
-    errorHandler
+  notFoundHandler,
+  errorHandler,
 } = require('./middlewares/common/errorHandler');
 const authRouter = require('./routers/authRouter');
-const {
-    authChecker
-} = require('./middlewares/auth/authMiddleware');
+const { authChecker } = require('./middlewares/auth/authMiddleware');
 const todoRouter = require('./routers/todoRouter');
 const Todo = require('./models/Todo');
 const indexController = require('./controllers/indexController');
 
-
 // initialization and config
 const app = express();
-dotenv.config()
-app.set('view engine', 'ejs')
-
+dotenv.config();
+app.set('view engine', 'ejs');
 
 // middlewares
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
-app.use(cookieParser(process.env.COOKIE_SECRETE))
+app.use(cookieParser(process.env.COOKIE_SECRETE));
 app.use(express.static('public'));
 
 // routers
-app.use(authRouter)
-app.use(todoRouter)
+app.use(authRouter);
+app.use(todoRouter);
 
-app.get('/', authChecker, indexController)
-
+app.get('/', authChecker, indexController);
 
 // not found handler
 app.use(notFoundHandler);
@@ -44,12 +41,16 @@ app.use(notFoundHandler);
 app.use(errorHandler);
 
 // mongoDB connection
-mongoose.connect(process.env.DB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        // app listing
-        app.listen(process.env.PORT || 3000, () => console.log("Server is running on port " + process.env.PORT))
-    })
-    .catch(err => console.log(err))
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false,
+  })
+  .then(() => {
+    // app listing
+    app.listen(process.env.PORT || 3000, () =>
+      console.log('Server is running on port ' + process.env.PORT)
+    );
+  })
+  .catch(err => console.log(err));
